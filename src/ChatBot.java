79eff8322efd38901e89992415cd9cb2dabeb80d
Chris <yuntian.chen.485@my.csun.edu>
2022-06-23 18:47:33 -0700

    new updates

---
+++ src/ChatBot.java
@@ -0,0 +1,15 @@
+public class ChatBot {
+    String name;
+
+    public ChatBot(){
+        name = "MACS";
+    }
+
+    /*methods
+     1. we need to figure out how we can process the request
+       --
+     */
+    public void response(String request){
+
+    }
+}
---
+++ src/datas.java
@@ -0,0 +1,10 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+public class datas extends ChatBot{
+    String file = "src//dataset.csv";
+    BufferedReader reader = null;
+    String line = "";
+
+
+}
